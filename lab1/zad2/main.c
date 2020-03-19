9632345844c2eec9a56090ee225e54cd06b11b86
Nananinana <nina.b746@gmail.com>
2020-03-19 01:01:43 +0100

    minor fixes task2

--- lab1/zad2/main.c
+++ lab1/zad2/main.c
@@ -28,11 +28,11 @@ void end_timer()
 
 void write_file_header(FILE *f)
 {
-    fprintf(f, "%30s\t\t%15s\t%15s\t%15s\t%15s\t%15s\n",
+    fprintf(f, "%30s\t\t%15s\t%15s\t%15s\n",
             "Name",
             "Real [s]",
             "User [s]",
-            "System [s]";
+            "System [s]");
 }
 
 void save_timer(char *name, FILE *f)
@@ -42,11 +42,11 @@ void save_timer(char *name, FILE *f)
     double real_time = (double)(en_time - st_time) / clk_tics;
     double user_time = (double)(en_cpu.tms_utime - st_cpu.tms_utime) / clk_tics;
     double system_time = (double)(en_cpu.tms_stime - st_cpu.tms_stime) / clk_tics;
-    fprintf(f, "%30s:\t\t%15f\t%15f\t%15f\t%15f\t%15f\t\n",
+    fprintf(f, "%30s:\t\t%15f\t%15f\t%15f\t\n",
             name,
             real_time,
             user_time,
-            system_time;
+            system_time);
 }
 int parse_create_main_table(char *argv[], int i, int argc)
 {
@@ -57,7 +57,8 @@ int parse_create_main_table(char *argv[], int i, int argc)
     }
     int size = atoi(argv[i + 1]);
 
-    return create_main_table(size);
+    create_main_table(size);
+    return 1;
 }
 int parse_end_timer(char *argv[], int i, int argc)
 {
@@ -73,7 +74,7 @@ int parse_end_timer(char *argv[], int i, int argc)
     return 0;
 }
 
-int parse_delete_block(char *argv[], int i, int argc)
+int parse_delete_block_of_operations(char *argv[], int i, int argc)
 {
 
     if (i + 1 >= argc)
@@ -82,9 +83,10 @@ int parse_delete_block(char *argv[], int i, int argc)
         return -1;
     }
     int index = atoi(argv[i + 1]);
-    delete_block(index);
+    delete_block_of_operations(index);
     return 1;
 }
+
 int parse_compare_pairs(char *argv[], int i, int argc)
 {
     if (i + 3 >= argc)
@@ -98,6 +100,7 @@ int parse_compare_pairs(char *argv[], int i, int argc)
     compare_pairs();
     return 1;
 }
+
 int parse_delete_operation(char *argv[], int i, int argc)
 {
     int block_index = atoi(argv[i + 1]);
