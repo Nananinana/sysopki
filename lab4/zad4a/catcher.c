149fdc258808ff49c2e504e35d01d7cbeb5d01cc
Nananinana <nina.b746@gmail.com>
2020-04-08 20:31:00 +0200

    zad4a added

---
+++ lab4/zad4a/catcher.c
@@ -0,0 +1,105 @@
+#define _XOPEN_SOURCE 500
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+typedef enum Mode
+{
+    KILL,
+    SIGQUEUE,
+    SIGRT
+} Mode;
+
+Mode mode;
+int sigusr1_signal;
+int sigusr2_signal;
+int received_signals = 0;
+
+void handler(int sig_no, siginfo_t *siginfo, void *ucontext)
+{
+    if (sig_no == sigusr1_signal)
+    {
+        received_signals++;
+        printf("Catcher: received so far %d signals from sender \n", received_signals);
+    }
+    else if (sig_no == sigusr2_signal)
+    {
+        if (mode == KILL || mode == SIGRT)
+        {
+            for (int i = 0; i < received_signals; ++i)
+                kill(siginfo->si_pid, sigusr1_signal);
+            kill(siginfo->si_pid, sigusr2_signal);
+        }
+        else
+        {
+            union sigval value;
+            for (int i = 0; i < received_signals; ++i)
+            {
+                value.sival_int = i;
+                sigqueue(siginfo->si_pid, sigusr1_signal, value);
+            }
+            sigqueue(siginfo->si_pid, sigusr2_signal, value);
+        }
+        printf("catcher received end signal. Number of signs received: %d \n", received_signals);
+        exit(0);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "wrong command, should be: mode (kill/sigqueque/sigrt) \n");
+        exit(1);
+    }
+
+    char *mode_name = argv[1];
+    if (strcmp("kill", mode_name) == 0)
+    {
+        mode = KILL;
+        sigusr1_signal = SIGUSR1;
+        sigusr2_signal = SIGUSR2;
+    }
+    else if (strcmp("queue", mode_name) == 0)
+    {
+        mode = SIGQUEUE;
+        sigusr1_signal = SIGUSR1;
+        sigusr2_signal = SIGUSR2;
+    }
+    else if (strcmp("sigrt", mode_name) == 0)
+    {
+        mode = SIGRT;
+        sigusr1_signal = SIGRTMIN + 1;
+        sigusr2_signal = SIGRTMAX - 1;
+    }
+    else
+    {
+        printf(stderr, "wrong command, should be: mode (kill/sigqueque/sigrt) \n");
+        exit(1);
+    }
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, sigusr1_signal);
+    sigdelset(&mask, sigusr2_signal);
+
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0)
+    {
+        printf("unable to block signal\n");
+        exit(1);
+    }
+    struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = handler;
+    sigemptyset(&act.sa_mask);
+    sigaddset(&act.sa_mask, sigusr1_signal);
+    sigaddset(&act.sa_mask, sigusr2_signal);
+    sigaction(sigusr1_signal, &act, NULL);
+    sigaction(sigusr2_signal, &act, NULL);
+
+    printf("catcher PID is: %d\n", getpid());
+    return 0;
+}
---
+++ lab4/zad4a/sender.c
@@ -0,0 +1,110 @@
+#define _XOPEN_SOURCE 500
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+typedef enum Mode
+{
+    KILL,
+    SIGQUEUE,
+    SIGRT
+} Mode;
+
+Mode mode;
+int number_of_signals;
+int received_signals;
+int sigusr1_signal;
+int sigusr2_signal;
+
+void handler(int sig_no, siginfo_t *siginfo, void *ucontext)
+{
+    if (sig_no == sigusr1_signal)
+    {
+        received_signals++;
+        printf("Sender: received so far %d signals from catcher \n", received_signals);
+        if (mode == SIGQUEUE)
+            printf("catcher index: %d\n", siginfo->si_value.sival_int);
+    }
+    else if (sig_no == sigusr2_signal)
+    {
+        printf("Sender received end signal. Number of signals received: %d, should have received: %d\n", received_signals, number_of_signals);
+        exit(0);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "wrong command, should be: catcher_PID number_of_signals_to_send mode (kill/sigqueque/sigrt) \n");
+    }
+    int catcher_PID = atoi(argv[1]);
+    number_of_signals = atoi(argv[2]);
+    char *mode_name = argv[3];
+
+    if (strcmp("kill", mode_name) == 0)
+    {
+        mode = KILL;
+        sigusr1_signal = SIGUSR1;
+        sigusr2_signal = SIGUSR2;
+    }
+    else if (strcmp("queue", mode_name) == 0)
+    {
+        mode = SIGQUEUE;
+        sigusr1_signal = SIGUSR1;
+        sigusr2_signal = SIGUSR2;
+    }
+    else if (strcmp("sigrt", mode_name) == 0)
+    {
+        mode = SIGRT;
+        sigusr1_signal = SIGRTMIN + 1;
+        sigusr2_signal = SIGRTMAX - 1;
+    }
+    else
+    {
+        fprintf(stderr, "wrong command, should be: catcher_PID number_of_signals_to_send mode (kill/sigqueque/sigrt) \n");
+        exit(1);
+    }
+
+    sigset_t mask;
+    sigfillset(&mask);
+    sigdelset(&mask, sigusr1_signal);
+    sigdelset(&mask, sigusr2_signal);
+    if (sigprocmask(SIG_BLOCK, &mask, NULL) < 0)
+    {
+        printf("unable to block signal \n");
+        exit(1);
+    }
+
+    struct sigaction act;
+    act.sa_flags = SA_SIGINFO;
+    act.sa_sigaction = handler;
+    sigemptyset(&act.sa_mask);
+    sigaddset(&act.sa_mask, sigusr1_signal);
+    sigaddset(&act.sa_mask, sigusr2_signal);
+    sigaction(sigusr1_signal, &act, NULL);
+    sigaction(sigusr2_signal, &act, NULL);
+
+    printf("sender has PID: %d\n", getpid());
+
+    if (mode == KILL || mode == SIGRT)
+    {
+        for (int i = 0; i < number_of_signals; ++i)
+            kill(catcher_PID, sigusr1_signal);
+        kill(catcher_PID, sigusr2_signal);
+    }
+    else
+    {
+        union sigval value;
+        value.sival_int = 0;
+        for (int i = 0; i < number_of_signals; ++i)
+            sigqueue(catcher_PID, sigusr1_signal, value);
+        sigqueue(catcher_PID, sigusr2_signal, value);
+    }
+
+    return 0;
+}
