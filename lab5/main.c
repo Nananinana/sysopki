18011bd6d50fac1f09c6328d91ab6e910f319f4c
Nananinana <nina.b746@gmail.com>
2020-04-21 16:53:23 +0200

    trying to fix pointers

--- lab5/main.c
+++ lab5/main.c
@@ -58,7 +58,7 @@ void find_arguments(char *task_arguments[], char *one_task)
     }
 }
 
-int parse_line (char *line, char **tasks[][])
+int parse_line (char *line, char **tasks)
 {
     char *line_copy = line;
     char *one_task = strtok_r(line, "|", &line_copy);
@@ -89,7 +89,7 @@ int main (int argc, char ** argv){
     char line[2048];
     
     while(fgets(line,2048,file)!=NULL){
-        &tasks_number = 0;
+        tasks_number = 0;
         char tasks[MAX_COMMANDS_IN_LINE][MAX_ARGS];
         for (int i = 0; i < MAX_COMMANDS_IN_LINE; ++i)
         {
