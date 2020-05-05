ed241eea4603221e092c61fcc613d2ade2384c6c
Nananinana <nina.b746@gmail.com>
2020-05-06 00:53:18 +0200

    rand changed

--- lab7/zad1/worker3.c
+++ lab7/zad1/worker3.c
@@ -66,7 +66,7 @@ int main()
     semaphore_id = get_semaphore();
     memory_id = get_shared_memory();
     while (1) {
-        usleep(4000);
+        usleep(10000);
         if (semctl(semaphore_id, 5, GETVAL, NULL) > 0)
             send_order();
     }
