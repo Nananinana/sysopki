2c313ae185a5c769543c16329c33b0bde32a0e74
Nananinana <nina.b746@gmail.com>
2020-05-06 00:55:55 +0200

    rand changed

--- lab7/zad1/worker1.c
+++ lab7/zad1/worker1.c
@@ -67,7 +67,7 @@ int main()
     memory_id = get_shared_memory();
 
     while (1) {
-        usleep(10000);
+        usleep(100000);
         if (semctl(semaphore_id, 3, GETVAL, NULL) + semctl(semaphore_id, 5, GETVAL, NULL) < MAX_ORDERS)
             add_order();
     }
